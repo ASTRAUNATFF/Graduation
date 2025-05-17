aca8a85419488bd739da21edd975b02df6294528
King Fat <120268913+ASTRAUNATFF@users.noreply.github.com>
2025-05-17 09:50:49 +0700

    17/05/2025
    Soucre Code Tổng Mới Nhất

--- Graduation/Kindergarten school/Kindergarten school/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Kindergarten_school.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
